dc86cbfe1b81c216cf628bc4bbb9e0a0f27280bb
Aleck Zhao <Aleck Zhao@AleckZhao-THINK.home>
2014-01-21 15:49:00 -0500

    finished? idk check it

--- Jumper/src/jumper/Jumper.java
+++ Jumper/src/jumper/Jumper.java
@@ -1,6 +1,84 @@
 package jumper;
 
-public class Jumper
-{
+import info.gridworld.actor.Actor;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+import java.awt.Color;
 
+public class Jumper extends Actor
+{
+	/**
+	 * Contructs a Jumper with a specified color
+	 * 
+	 * @param color
+	 *            the color of the Jumper
+	 */
+	public Jumper(Color color)
+	{
+		super();
+		super.setColor(color);
+	}
+
+	/**
+	 * makes the Jumper act
+	 */
+	public void act()
+	{
+		if (canMove())
+			move();
+		else
+			turn();
+	}
+
+	/**
+	 * turns the bug 45 degrees to the right
+	 */
+	public void turn()
+	{
+		super.setDirection(getDirection() + Location.HALF_RIGHT);
+	}
+
+	/**
+	 * test first if grid is valid. then test if two spaces ahead is valid. if
+	 * not, that space must be out of the grid, and then test if one space ahead
+	 * is valid. if so, move there. otherwise, turn.
+	 */
+	public void move()
+	{
+		Grid<Actor> gr = getGrid();
+		if (gr == null)
+			return;
+
+		Location loc = getLocation();
+		Location next = loc.getAdjacentLocation(getDirection());
+		Location second = next.getAdjacentLocation(getDirection());
+
+		if (gr.isValid(second))
+			moveTo(second);
+		else if (gr.isValid(next))
+			moveTo(next);
+		else
+			removeSelfFromGrid();
+	}
+
+	/**
+	 * returns true if the jumper can move
+	 * 
+	 * @return true if the jumper can move
+	 */
+	public boolean canMove()
+	{
+		Grid<Actor> gr = getGrid();
+
+		if (gr == null)
+			return false;
+
+		Location loc = getLocation();
+		Location next = loc.getAdjacentLocation(getDirection());
+
+		if (!gr.isValid(next))
+			return false;
+
+		return true;
+	}
 }
---
+++ Jumper/src/jumper/JumperRunner.java
@@ -0,0 +1,38 @@
+package jumper;
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.ActorWorld;
+import info.gridworld.actor.Flower;
+import info.gridworld.actor.Rock;
+import info.gridworld.grid.Location;
+import java.awt.Color;
+
+public class JumperRunner
+{
+	public static void main(String[]args)
+	{
+		ActorWorld world = new ActorWorld();
+		
+		Jumper jesseParks = new Jumper(Color.PINK);
+		Jumper eaten=new Jumper(Color.CYAN);
+		eaten.setDirection(Location.EAST);
+		
+		Jumper dodge = new Jumper(Color.MAGENTA);
+		dodge.setDirection(Location.WEST);
+		
+		world.add(new Location(6,4), jesseParks);
+		world.add(new Location(3,4),new Rock());
+		world.add(new Location(2,4),new Rock());
+		world.add(new Location(0,5),new Flower());
+		world.add(new Location(0,6),new Flower());
+		world.add(new Location(0,9),new Rock());
+		world.add(new Location(8,9),new Flower());
+		world.add(new Location(9,9),new Flower());
+		world.add(new Location(9,8), new Actor());
+		world.add(new Location(9,7), new Actor());
+		world.add(new Location(4,2),eaten);
+		world.add(new Location(2,7),dodge);
+		
+		world.show();
+	}
+}
