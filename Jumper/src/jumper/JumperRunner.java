7999c694010a93780a57a3b98662139c52e50b83
Aleck Zhao <Aleck Zhao@AleckZhao-THINK.home>
2014-01-21 15:49:17 -0500

    finished? idk check it

--- Jumper/src/jumper/JumperRunner.java
+++ Jumper/src/jumper/JumperRunner.java
@@ -9,30 +9,35 @@ import java.awt.Color;
 
 public class JumperRunner
 {
-	public static void main(String[]args)
+	public static void main(String[] args)
 	{
 		ActorWorld world = new ActorWorld();
-		
+
 		Jumper jesseParks = new Jumper(Color.PINK);
-		Jumper eaten=new Jumper(Color.CYAN);
+		Jumper eaten = new Jumper(Color.CYAN);
 		eaten.setDirection(Location.EAST);
-		
-		Jumper dodge = new Jumper(Color.MAGENTA);
-		dodge.setDirection(Location.WEST);
-		
-		world.add(new Location(6,4), jesseParks);
-		world.add(new Location(3,4),new Rock());
-		world.add(new Location(2,4),new Rock());
-		world.add(new Location(0,5),new Flower());
-		world.add(new Location(0,6),new Flower());
-		world.add(new Location(0,9),new Rock());
-		world.add(new Location(8,9),new Flower());
-		world.add(new Location(9,9),new Flower());
-		world.add(new Location(9,8), new Actor());
-		world.add(new Location(9,7), new Actor());
-		world.add(new Location(4,2),eaten);
-		world.add(new Location(2,7),dodge);
-		
+
+		Jumper dodged = new Jumper(Color.MAGENTA);
+		dodged.setDirection(Location.WEST);
+
+		world.add(new Location(6, 4), jesseParks);
+		world.add(new Location(4, 2), eaten);
+		world.add(new Location(2, 7), dodged);
+
+		world.add(new Location(3, 4), new Rock());
+		world.add(new Location(2, 4), new Rock());
+
+		world.add(new Location(0, 5), new Flower());
+		world.add(new Location(0, 6), new Flower());
+
+		world.add(new Location(0, 9), new Rock());
+
+		world.add(new Location(8, 9), new Flower());
+		world.add(new Location(9, 9), new Flower());
+
+		world.add(new Location(9, 8), new Actor());
+		world.add(new Location(9, 7), new Actor());
+
 		world.show();
 	}
 }
